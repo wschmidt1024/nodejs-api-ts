b1553e605cec676f6023c4a8e3fb58d4ca12b395
wschmidt1024 <45727806+wschmidt1024@users.noreply.github.com>
2022-03-21 18:37:58 -0600

    Basic Unit Tests
    Packages
    * Adds Jest, ts-jest, and the TypeScript types.
    * Updates the test script to build the project before testing (with coverage).
    
    tsconfig.json
    * Adds setting to exclude the tests folder.
    
    Jest
    * Adds the basic Jest config file.
    * Adds the preset 'ts-jest'
    
    Mongoose mapper
    * Updates function to not return null.
    
    Mongooose Mapper test
    * Adds the happy path test.

--- src/database/mongoose/helpers/mappers.ts
+++ src/database/mongoose/helpers/mappers.ts
@@ -2,13 +2,13 @@ import { User } from '../../models/user';
 
 import { IUserDocument } from '../schemas/user.schema';
 
-export function UserDocument2User(model: IUserDocument): User | null {
-	return model ? {
+export function UserDocument2User(model: IUserDocument): User {
+	return {
 		id: model._id.toString(),
 		first: model.first,
 		last: model.last,
 		email: model.email,
 		username: model.username,
 		password: model.password
-	} : null;
+	};
 }
